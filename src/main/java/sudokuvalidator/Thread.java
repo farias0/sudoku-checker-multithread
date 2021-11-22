9052ae0b13edfbe9c9c4e5f58a40ff720befbe7a
farias <fariasjunior96@gmail.com>
2021-11-22 11:43:10 -0300

    multithreaded checking working

--- src/main/java/sudokuvalidator/Thread.java
+++
@@ -1,9 +0,0 @@
-package sudokuvalidator;
-
-public class Thread extends java.lang.Thread {
-
-  @Override
-  public void run() {
-    System.out.println("Thread " + Thread.class.getName() + " ran.");
-  }
-}
